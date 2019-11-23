4d04ccdf6ab37eb6f93ddb4bb9b11a4162b06ea8
HenleyZ <zhoulin1620@163.com>
2019-11-23 10:21:40 +0800

    iconfont 文件路径设置

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    <div class="home">home</div>
+    <home-header />
   </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
 };
 </script>
 
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,57 @@
+
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入景点旅游
+    </div>
+    <div class="header-right">
+      城市<span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
+    <div class="test"></div>
+  </div>
+</template>
+
+<script>
+// 1rem = 50 px
+export default {
+  name: 'HomeHeader'
+};
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header
+    display flex
+    line-height .86rem
+    background-color $bgColor
+    color #fff
+    .header-left
+      width .64rem
+      float left
+      .back-icon
+        text-align center
+        font-size .4rem
+    .header-input
+      height .64rem
+      background-color #fff
+      border-radius .1rem
+      padding-left .2rem
+      margin-top .12rem
+      line-height 0.64rem
+      margin-left .2rem
+      flex 1
+      color #ccc
+    .header-right
+      width 1.24rem
+      float right
+      text-align center
+      .arrow-icon
+        margin-left .04rem
+        font-size .24rem
+
+
+</style>
