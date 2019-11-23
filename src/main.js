610f82da84208459f4e26eff207de8c6926fb2a4
HenleyZ <zhoulin1620@163.com>
2019-11-23 14:25:10 +0800

    swiper img

--- src/main.js
+++ src/main.js
@@ -1,14 +1,25 @@
+/*
+ * @Author: zhoulin
+ * @Date: 2019-11-18 10:09:55
+ * @LastEditors: your name
+ * @LastEditTime: 2019-11-23 14:23:03
+ * @Description: file content
+ */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper, /* { default global options } */)
 
 /* eslint-disable no-new */
 new Vue({
