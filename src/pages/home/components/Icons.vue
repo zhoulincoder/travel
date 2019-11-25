96e010a7b051e82f88b9dc03a17240aa86e92371
HenleyZ <zhoulin1620@163.com>
2019-11-25 09:04:10 +0800

    add icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.url">
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [
+        {
+          id: '0001',
+          url: 'https://s.qunarzz.com/homenode/images/touchheader/piao.png',
+          desc: '景点门票'
+        },
+        {
+          id: '0002',
+          url: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png',
+          desc: '酒店'
+        },
+        {
+          id: '0003',
+          url: 'https://s.qunarzz.com/homenode/images/touchheader/flight.png',
+          desc: '机票'
+        },
+        {
+          id: '0004',
+          url: 'https://s.qunarzz.com/homenode/images/touchheader/package.png',
+          desc: '度假'
+        },
+        {
+          id: '0005',
+          url: 'https://s.qunarzz.com/homenode/images/touchheader/train.png',
+          desc: '火车票'
+        },
+        {
+          id: '0006',
+          url: 'https://s.qunarzz.com/homenode/images/touchheader/piao.png',
+          desc: '景点门票2'
+        },
+        {
+          id: '0007',
+          url: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png',
+          desc: '酒店2'
+        },
+        {
+          id: '0008',
+          url: 'https://s.qunarzz.com/homenode/images/touchheader/flight.png',
+          desc: '机票2'
+        },
+        {
+          id: '0009',
+          url: 'https://s.qunarzz.com/homenode/images/touchheader/package.png',
+          desc: '度假2'
+        },
+        {
+          id: '0010',
+          url: 'https://s.qunarzz.com/homenode/images/touchheader/train.png',
+          desc: '火车票2'
+        }
+      ]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8) // 图片显示的页码
+        if(!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles'
+@import '~styles/mixins'
+  .icons >>> .swiper-container
+    height 0
+    padding-bottom 50%
+  .icon
+    float left
+    width 25%
+    padding-bottom 25%
+    // background red
+    position relative
+    height 0
+    .icon-img
+      position absolute
+      top 0
+      left 0
+      right 0
+      bottom .44rem
+      // background blue
+      box-sizing border-box
+      padding .1rem
+      .icon-img-content
+        display block
+        margin 0 auto 
+        height 100%
+    .icon-desc
+      ellipsis()
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      height 0.44rem
+      line-height .44rem
+      color $darkTextColor
+      text-align center
+</style>
