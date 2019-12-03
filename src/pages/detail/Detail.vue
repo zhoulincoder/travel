f31b44504ee67d1807ebf385052cbc4a6f44a99a
HenleyZ <zhoulin1620@163.com>
2019-12-03 14:39:39 +0800

    递归组件的使用

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,18 +2,53 @@
   <div>
     <detail-banner />
     <detail-header />
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :list="list"/>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [
+        {
+          title: '成人票',
+          children: [
+            {
+              title: '成人三馆连票',
+              children: [
+                {
+                  title: '成人三馆连票 某一个连锁店'
+                }
+              ]
+            },
+            {
+              title: '成人五馆连票'
+            }
+          ]
+        },
+        {
+          title: '学生票'
+        },
+        {
+          title: '儿童票'
+        },
+        {
+          title: '特惠票'
+        }
+      ]
+    }
   }
 }
 </script>
