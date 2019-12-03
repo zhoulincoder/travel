bd19c71cdcfc9d41e4e313ee4417fd58873e9524
HenleyZ <zhoulin1620@163.com>
2019-12-03 14:17:15 +0800

    update

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -26,6 +26,9 @@ export default {
   activated () {
     window.addEventListener('scroll', this.handleScroll)
   },
+  deactivated () {
+    window.removeEventListener('scroll', this.handleScroll)
+  },
   methods: {
     handleScroll () {
       let top = document.documentElement.scrollTop
